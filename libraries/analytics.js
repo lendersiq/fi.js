687d3b48c765dec1844760d32462c69c792e7e10
lendersiq <lendersiq@gmail.com>
2024-08-09 06:47:45 -0400

    Create analytics.js
    initial commit

---
+++ libraries/analytics.js
@@ -0,0 +1,9 @@
+const analytics = {
+    attributes: {
+        taxRate: {
+            description: "The institution tax rate applied to financial calculations",
+            value: 0.27
+        },
+    }
+};
+window.analytics = analytics;
